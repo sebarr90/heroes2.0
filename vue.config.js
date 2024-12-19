d88d4b52bbc2a53e07b2409ef92368c8ef70f490
sebarr90 <arriamarcos@gmail.com>
2024-12-19 17:41:37 -0300

    aw modifica vue-config

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
 
-  pluginOptions: {
-    vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
-})
+module.exports = {
+ publicPath: '/heroes2.0'
+}
